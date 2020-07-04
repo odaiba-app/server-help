f4dd6830993a597aa59f50935e5403b256d341d7
dzakki <dzakkiaz7@gmail.com>
2020-07-04 17:57:51 +0700

    fix bug

--- app.js
+++ app.js
@@ -56,6 +56,11 @@ let db = {
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+
+  socket.on("joinRoom", function(room) {
+    socket.join(room);
+  });
+
   socket.on("getGroups", function(to) {
     realtimeGroup();
     console.log("masuk g?");
@@ -84,12 +89,12 @@ io.on("connection", (socket) => {
   socket.on("updateSheet", function(response) {
     const { sheets, groupid } = response;
     const indexGroup = db.groups.findIndex(function(r) {
-      return r.id === Number(groupid);
+      return r.id === Number(response.groupid);
     });
     console.log(indexGroup, "indexGroup");
-    db.groups[indexGroup].sheets = sheets;
+    db.groups[indexGroup].sheets = response.sheets;
     // console.log(db.groups[indexGroup].sheets)
-    if (sheets.length) {
+    if (response.sheets.length) {
       realtimeGroup();
     }
     console.log(db.groups[0].answered);
@@ -98,7 +103,7 @@ io.on("connection", (socket) => {
   });
   socket.on("answerQ", function(response) {
     console.log(response);
-    socket.broadcast.emit("answerQ", response);
+    socket.broadcast.to(response.room).emit("answerQ", response);
     realtimeGroup();
     io.emit("realtime-groups", db.groups);
   });
